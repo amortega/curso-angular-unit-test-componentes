142608a0d54dfbd63dc9a738bae6fcb189dd681d
Andrés Ortega <arguello.25018@gmail.com>
2023-10-02 13:13:58 -0300

    add tests

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+import { PeopleComponent } from './components/people/people.component';
 import { ProductsComponent } from './components/products/products.component';
 import { PicoPreviewComponent } from './components/pico-preview/pico-preview.component';
+import { OthersComponent } from './components/others/others.component';
 
 const routes: Routes = [
   {
@@ -12,6 +14,14 @@ const routes: Routes = [
   {
     path: 'pico-preview',
     component: PicoPreviewComponent
+  },
+  {
+    path: 'people',
+    component: PeopleComponent
+  },
+  {
+    path: 'others',
+    component: OthersComponent
   }
 ];
 
